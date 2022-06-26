82f20008f6dc817fb8757273f061a90b1da862f1
lauralachkar <99486645+lauralachkar@users.noreply.github.com>
2022-06-26 12:48:04 +0300

    Add files via upload

---
+++ Exercise1-Introduction-JS-week2-day1.js
@@ -0,0 +1,6 @@
+let addressNumber = "Heleni Ha-Malka Street";
+let addressStreet = 16;
+let country = "Israel";
+
+let globalAddress = " I live in " + "" + addressNumber + "" + addressStreet + "," + "" + country
+console.log(globalAddress)
