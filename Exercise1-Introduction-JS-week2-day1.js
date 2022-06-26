a7ec4faf737ec61d659418ee8873e4962d275184
lauralachkar <99486645+lauralachkar@users.noreply.github.com>
2022-06-26 13:28:09 +0300

    Delete Exercise1-Introduction-JS-week2-day1.js

--- Exercise1-Introduction-JS-week2-day1.js
+++
@@ -1,6 +0,0 @@
-let addressNumber = "Heleni Ha-Malka Street";
-let addressStreet = 16;
-let country = "Israel";
-
-let globalAddress = " I live in " + "" + addressNumber + "" + addressStreet + "," + "" + country
-console.log(globalAddress)
