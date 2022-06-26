8549004599ac9fdf1a0511e7506b557f792c77bc
lauralachkar <99486645+lauralachkar@users.noreply.github.com>
2022-06-26 13:27:33 +0300

    Add files via upload

---
+++ Exercises-Introduction-JS-week2-day1.js
@@ -0,0 +1,19 @@
+// First Exercise
+
+let addressNumber = "Heleni Ha-Malka Street";
+let addressStreet = 16;
+let country = "Israel";
+
+let globalAddress = " I live in " + "" + addressNumber + "" + addressStreet + "," + "" + country
+console.log(globalAddress)
+
+
+// Third Exercise
+
+let arr = ["cat","dog","fish","rabbit","cow"];
+let getCellValue = arr[1]
+console.log(getCellValue)
+arr.push("horse");
+console.log(arr)
+let arrLength = arr.length;
+console.log(arrLength)
